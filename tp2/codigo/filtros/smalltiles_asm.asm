59a5ba060db13a3e2fa454ffe879d3f7e616e0eb
Asdolo <lraposeiras@dc.uba.ar>
2016-09-08 23:11:43 -0300

    empezando con smalltiles_asm
    Lo que hicimos en la Noriega el 08/09/2016

--- tp2/codigo/filtros/smalltiles_asm.asm
+++ tp2/codigo/filtros/smalltiles_asm.asm
@@ -3,6 +3,24 @@ DEFAULT REL
 
 section .text
 global smalltiles_asm
+
+; void smalltiles_asm    (unsigned char *src, unsigned char *dst, int cols, int filas,
+;                     int src_row_size, int dst_row_size);
 smalltiles_asm:
 ;COMPLETAR
+	mov r8, rdi					; r8 <- src
+	mov r9, rsi					; r9 <- dst
+
+	xor rdx, rdx				; limpio el contador
+
+
+ciclo:
+	pmovupdw xmm0, [r8]
+	add r8,8
+	pmovupdw xmm1, [r8]
+	slr xmm1
+	
+
+
+	loop
 	ret
