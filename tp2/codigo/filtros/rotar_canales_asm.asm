057e55157c09f12087b1b6414dbcb90289566ecb
Federico Alliani <fedealliani@gmail.com>
2016-09-12 20:03:57 -0300

    Filtro rotar 100% hecho en C y ASM

--- tp2/codigo/filtros/rotar_canales_asm.asm
+++ tp2/codigo/filtros/rotar_canales_asm.asm
@@ -3,10 +3,53 @@ DEFAULT REL
 
 
 
+section .rodata
+mascara: db 0x01, 0x02, 0x00, 0x03, 0x05, 0x06, 0x04, 0x07, 0x09, 0x0A, 0x08, 0x0B, 0x0D, 0x0E, 0x0C, 0x0F
 
 section .text
 global rotar_asm
 rotar_asm:
 ;COMPLETAR
+;void rotar_asm    (unsigned char *src, unsigned char *dst, int cols, int filas,int src_row_size,int dst_row_size);
+;HACER PUSH DE R12 Y R13
+
+push rbp		; A
+mov rbp, rsp
+push rbx		; D
+push r12		; A
+push r13		; D
+sub rsp, 8		; A
+
+mov r10, rdi					; r10 <- src
+mov r11, rsi					; r11 <- dst r9 
+mov r12,rdx					;r12 <- #columnas
+mov r13,rcx					; r13 <- #filas
+
+
+xor rcx, rcx			; limpio rcx
+mov eax, r13d
+shr eax, 2
+mul r12d	
+
+mov ecx, eax 			; ecx = cantidad de pixels (filas * cols)
+movdqu xmm8, [mascara]
+
+.ciclo:
+	
+movdqu xmm0, [r10]	; xmm0 = |a3 r3 g3 b3|a2 r2 g2 b2|a1 r1 g1 b1|a0 r0 g0 b0|
+	pshufb	xmm0, xmm8	; xmm0 = |a3 g3 b3 r3|a2 g2 b2 r2|a1 g1 b1 r1|a0 g0 b0 r0|
+	
+	movdqu [r11], xmm0
+
+	add r10, 16
+	add r11, 16
+
+	loop .ciclo
+
+add rsp,8 ;d
+pop r13 ;a
+pop r12	;d
+pop rbx	;a
+pop rbp	;d
 		
-	ret
+ret	;a
--- tp2/codigo/filtros/smalltiles_asm.asm
+++ tp2/codigo/filtros/smalltiles_asm.asm
@@ -8,50 +8,44 @@ global smalltiles_asm
 ;                     int src_row_size, int dst_row_size);
 smalltiles_asm:
 ;COMPLETAR
-	mov r8, rdi					; r8 <- src
-	mov r9, rsi					; r9 <- dst r9 abajo la izquierda
+	;mov r8, rdi					; r8 <- src
+	;mov r9, rsi					; r9 <- dst r9 abajo la izquierda
 	;r10=r9+(cols/2)
 	;r10 abajo a la derecha
 	;r11=r9+(filas/2)
 	;r11 arriba a la izquierda
 	;r12=r9++(cols/2)+(filas/2)
 	;r12 arriba derecha
-	xor rdx, rdx				; limpio el contador
-
-while(j<altura)
-for(i=0;j<ancho;j++){
-	if(j+1==ancho){
-	j=0;
-	altura++;
-	}
-}
+	;xor rdx, rdx				; limpio el contador
 
 
 
-	mov xmm8,[masc1] ;XMM8 ES LA MASCARA
+
+
+	;mov xmm8,[masc1] ;XMM8 ES LA MASCARA
 ciclo:
-	pmovupdw xmm0, [r8] 
+	;pmovupdw xmm0, [r8] 
 	;xmm0= |--|p2|--|p0|
 
 	;xmm1= |--|p10|--|p8|
-	movupdw xmm1, [r8+??]
+	;movupdw xmm1, [r8+??]
 
-	add r8,16*ancho/2
+	;add r8,16*ancho/2
 
-	mov xmm2,xmm0
+	;mov xmm2,xmm0
 	;xmm2= |--|p2|--|p0|
 
-	pshufd xmm2,xmm0,00001000b
+	;pshufd xmm2,xmm0,00001000b
 	;xmm2= |--|--|p2|p0|
 
-	pshufd xmm3,xmm1,b
+	;pshufd xmm3,xmm1,b
 	;xmm3= |p10|p8|--|--|
 
-	and xmm2,FFFFFFFFH,FFFFFFFFH,00000000h,00000000h
+	;and xmm2,FFFFFFFFH,FFFFFFFFH,00000000h,00000000h
 	;xmm2=|0|0|p2|p0
-	and xmm3,00000000h,00000000h,FFFFFFFFH,FFFFFFFFH
+	;and xmm3,00000000h,00000000h,FFFFFFFFH,FFFFFFFFH
 	;xmm3=|p10|p8|0|0
-	padddw xmm3,xmm2
+	;padddw xmm3,xmm2
 	;xmm3=|p10|p8|p2|p0
 
 
@@ -59,5 +53,5 @@ ciclo:
 	;mov [r9],xmm2 ... Escribimos en destino
 	;add r9,16 Incrementamos el destino
 
-	loop
-	ret
+	;loop
+	;ret
