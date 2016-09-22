b72a907c2e41dbb1c7462968b0586e38e46ee486
Raposeiras Lucas Damian <lraposeiras@ws10.labo5>
2016-09-22 17:03:29 -0300

    asd

--- tp2/codigo/filtros/colorizar_asm.asm
+++ tp2/codigo/filtros/colorizar_asm.asm
@@ -20,255 +20,260 @@
 
 global colorizar_asm
 
-section .rodata
-    uno:   dd 1.0, 1.0, 1.0, 1.0
-    
-section .text
-
-
-
-
+; section .rodata
+;     uno:   dd 1.0, 1.0, 1.0, 1.0
+;     
+; section .text
+; 
+; 
+; 
+; 
 colorizar_asm:
 
 
 	; r10 va a ser una constante = ancho*4
     ; r11 va a ser una constante = ancho/4
     ; r8 va a ser una constante = (ancho*4)*3
-    xor r9, r9                      ; r9 = 0
-    mov r9d, ecx                    ; r9 = alto
-
-    xor r10, r10   		            ; r10 =  0
-    mov r10d, edx  		            ; r10 = ancho
-    shl r10, 2     		            ; r10 = ancho*4
-    
-    xor r8, r8
-    mov rax, r10					; rax = ancho*4
-    mul 3							; rax = (ancho*4)*3
-    mov r8, rax						; r8 = (ancho*4)*3
-    
-    xor r11, r11                 	; r10 =  0
-    mov r11d, edx                	; r10 = ancho
-    shr r11, 2						; r10 = ancho/4
-    
-
-	; movemos rsi a la fila de arriba
-	add rsi, r10
-
-
-	; r9 va a ser un iterador = alto
-    xor r9, r9                              ; r9 = 0
-    mov r9d, ecx                            ; r9 = alto
-
-    ; seteo el contador
-    mov rcx, r11                            ; rbx = ancho/4
-    
-    ; xmm0 =  |           |           |           |   alpha   |
-    pshufd xmm0, 0000000000b
-    ; xmm0 =  |   alpha   |   alpha   |   alpha   |   alpha   |
-		
-	movups xmm10, [uno]
-	; xmm10 = |    1.0    |    1.0    |    1.0    |    1.0    |
-	
-	addps xmm10, xmm0	
-	; xmm10 = | 1 + alpha | 1 + alpha | 1 + alpha | 1 + alpha |
-	
-	movups xmm11, [uno]
-	; xmm11 = |    1.0    |    1.0    |    1.0    |    1.0    |
-	
-	subps xmm11, xmm0	
-    ; xmm11 = | 1 - alpha | 1 - alpha | 1 - alpha | 1 - alpha |
-	
- .ciclo:
-
- 	movdqu xmm0, [rdi]
- 	; xmm0 = | A[0][3] | R[0][3] | G[0][3] | B[0][3] | A[0][2] | R[0][2] | G[0][2] | B[0][2] | A[0][1] | R[0][1] | G[0][1] | B[0][1] | A[0][0] | R[0][0] | G[0][0] | B[0][0] |
-    ;         127       119       111       103       95        87        79        71        63        55        47        39        31        23        15        7       0
-	
-	movdqu xmm1, [rdi + r10]
-	; xmm1 = | A[1][3] | R[1][3] | G[1][3] | B[1][3] | A[1][2] | R[1][2] | G[1][2] | B[1][2] | A[1][1] | R[1][1] | G[1][1] | B[1][1] | A[1][0] | R[1][0] | G[1][0] | B[1][0] |
-    ;         127       119       111       103       95        87        79        71        63        55        47        39        31        23        15        7       0
-    
-	movdqu xmm2, [rdi + 2*r10]
-	; xmm2 = | A[2][3] | R[2][3] | G[2][3] | B[2][3] | A[2][2] | R[2][2] | G[2][2] | B[2][2] | A[2][1] | R[2][1] | G[2][1] | B[2][1] | A[2][0] | R[2][0] | G[2][0] | B[2][0] |
-    ;         127       119       111       103       95        87        79        71        63        55        47        39        31        23        15        7       0
-    
-    pmaxub xmm1,xmm2
-	; xmm1 = | max(pix[1][3], pix[2][3]) | max(pix[1][2], pix[2][2]) | max(pix[1][1], pix[2][1]) | max(pix[1][0], pix[2][0]) |
-	
-	
-	; backupeo las 3 filas porque necesito los pixels del medio
-	movdqu xmm3, xmm0
-	; xmm3 = | A[0][3] | R[0][3] | G[0][3] | B[0][3] | A[0][2] | R[0][2] | G[0][2] | B[0][2] | A[0][1] | R[0][1] | G[0][1] | B[0][1] | A[0][0] | R[0][0] | G[0][0] | B[0][0] |
-    ;         127       119       111       103       95        87        79        71        63        55        47        39        31        23        15        7       0
-	
-	movdqu xmm4, xmm1
-	; xmm4 = | A[1][3] | R[1][3] | G[1][3] | B[1][3] | A[1][2] | R[1][2] | G[1][2] | B[1][2] | A[1][1] | R[1][1] | G[1][1] | B[1][1] | A[1][0] | R[1][0] | G[1][0] | B[1][0] |
-    ;         127       119       111       103       95        87        79        71        63        55        47        39        31        23        15        7       0
-    
-	movdqu xmm5, xmm2
-	; xmm5 = | A[2][3] | R[2][3] | G[2][3] | B[2][3] | A[2][2] | R[2][2] | G[2][2] | B[2][2] | A[2][1] | R[2][1] | G[2][1] | B[2][1] | A[2][0] | R[2][0] | G[2][0] | B[2][0] |
-    ;         127       119       111       103       95        87        79        71        63        55        47        39        31        23        15        7       0
-    
-	
-	; calculo maximos por columnas
-	pmaxub xmm0,xmm1
-	; xmm0 = | max(pix[0][3], pix[1][3], pix[2][3]) | max(pix[0][2], pix[1][2], pix[2][2]) | max(pix[0][1], pix[1][1], pix[2][1]) | max(pix[0][0], pix[1][0], pix[2][0]) |
-	; xmm0 = | max(col3]) | max(col2) | max(col1) | max(col0) |
- 	
- 	movdqu xmm1, xmm0
- 	; xmm1 = | max(col3) | max(col2) | max(col1) | max(col0) |
- 	
-	pshufb xmm0, [maxCol0y2_maxCol1y3]
-	; xmm0 = |     0     |     0     | max(col3) | max(col2)  |
-
-	pmaxub xmm0, xmm1
-	; xmm0 = |   fruta   |   fruta   | max(col1, col3) | max(col0, col2) |
-	
-	
-	; ahora acomodo mis backups xmm3 y xmm5 para calcular los maximos con los pixeles del medio
-	; quiero maximo entre: max(col0, col2), xmm3[1] y xmm5[1] (donde xmmn[k] es el k-ésimo pixel del registro xmmn)
-	; y ademas quiero maximo entre: max(col1, col3), xmm3[2] y xmm5[2] (donde xmmn[k] es el k-ésimo pixel del registro xmmn)
-	; entiéndase que anoto pixels, pero los máximos son realmente a nivel byte/componente
-	
-	; tengo:
-	; xmm0 = |   fruta   |   fruta   | max(col1, col3) | max(col0, col2) |
-	; xmm3 = |  xmm3[3]  |  xmm3[2]  |     xmm3[1]     |     xmm3[0]     |
-	; xmm5 = |  xmm5[3]  |  xmm5[2]  |     xmm5[1]     |     xmm5[0]     |
-	
-	; entonces, acomodo mis backups para usar pmaxub nuevamente:
-	
-	movdqu xmm6, xmm3
-	; xmm6 = |  xmm3[3]  |  xmm3[2]  |     xmm3[1]     |     xmm3[0]     |
-	
-	pshufb xmm6, [maxPixelsMedio]
-	; xmm6 = |     0     |     0     |     xmm3[2]     |     xmm3[1]     |
-	
-	pmaxub xmm0, xmm6
-	; xmm0 = |   fruta   |   fruta   | max(col1, col3, xmm3[2]) | max(col0, col2, xmm3[1]) |
-	
-	movdqu xmm6, xmm5
-	; xmm6 = |  xmm5[3]  |  xmm5[2]  |     xmm5[1]     |     xmm5[0]     |
-	
-	pshufb xmm6, [maxPixelsMedio]
-	; xmm6 = |     0     |     0     |     xmm5[2]     |     xmm5[1]     |
-	
-	pmaxub xmm0, xmm6
-	; xmm0 = |   fruta   |   fruta   | max(col1, col3, xmm3[2], xmm5[2]) | max(col0, col2, xmm3[1], xmm5[1]) |
-	
-	; tengo:
-	; xmm0 = |   fruta   |   fruta   | max*(p1) | max*(p0) |
-	; xmm0 = |   fruta   |   fruta   | maxA1 | maxR1 | maxG1 | maxB1 | maxA0 | maxR0 | maxG0 | maxB0 |
-	;                                 63      55      47      39      31      23      15      7     0
-	
-	; if (maxR >= maxG && maxR >= maxB){
-	; 	res = 1 + alpha;
-	; }
-	; else
-	; {
-	; 		res = 1 - alpha;
-	; }
-	
-	; if (maxR < maxG && maxG >= maxB){
-	; 	res = 1 + alpha;
-	; }
-	; else
-	; {
-	; 		res = 1 - alpha;
-	; }
-	
-	; if (maxR < maxB && maxG < maxB){
-	; 	res = 1 + alpha;
-	; }
-	; else
-	; {
-	; 		res = 1 - alpha;
-	; }
-
-	
-	
-	; calculemos phiR, phiG, phiB
-	
-	; reacomodo xmm0 para aprovechar mas comparaciones
-	
-	pshufb xmm0, [mask1]
-	; xmm0 = |     0     |     0     | maxB1 | maxR1 | maxG1 | maxB1 | maxB0 | maxR0 | maxG0 | maxB0 |
-	;                                 63      55      47      39      31      23      15      7     0
-	
-	movdqu xmm1, xmm0
-	; xmm1 = |     0     |     0     | maxA1 | maxR1 | maxG1 | maxB1 | maxA0 | maxR0 | maxG0 | maxB0 |
-	;                                 63      55      47      39      31      23      15      7     0
-	
-	pshufb xmm1, [mask2]
-	; xmm1 = |     0     |     0     | maxG1 | maxG1 | maxR1 | maxR1 | maxG0 | maxG0 | maxR0 | maxR0 |
-	;                                 63      55      47      39      31      23      15      7     0
-	
-	pcmpgtb xmm0, xmm1	; xmm0 > xmm1
-	
-	; xmm0 = |         0         | maxB1 > maxG1 | maxR1 > maxG1 | maxG1 > maxR1 | maxB1 > maxR1 | maxB0 > maxG0 | maxR0 > maxG0 | maxG0 > maxR0 | maxB0 > maxR0 |
-	;         127                 63              55              47              39              31              23              15             7              0
-	
-	; niego la mascara, porque así me sirve para phiR
-	mov xmm2, xmm0
-	xor xmm2, [maskUnos]
-	
-	; xmm4 = | maxG1 <= maxR1 | maxB1 <= maxR1 | maxB1 <= maxG1 | maxG1 <= maxB1| basurax4 | maxG0 <= maxR1| maxB0 <= maxR1| maxB0 <= maxG1 | maxG0 <= maxB1 | basurax4 |
-	
-	
-	mov xmm6, xmm4				;xmm6 = xmm4
-	mov xmm7,xmm4				; xmm7 = xmm4
-	pshufb xmm6,[mascaraYaNoSeQueNombrePonerle1]
-		pshufb xmm7,[mascaraYaNoSeQueNombrePonerle2]
-	;xmm6 = | | | maxB1 <= maxR1 | maxB0 <= maxR1| (todo en bytes)
-	;xmm7 = | | | maxG1 <= maxR1 | maxG0 <= maxR1| (todo en bytes)
-	
-	
-	; xmm15 = | 1 + alpha | 1 - alpha | 1 + alpha | 1 - alpha |
-	
-	
-	
-	pand xmm6,xmm7
-	;xmm6 = | | | (maxB1 <= maxR1) & (maxG1 <= maxR1) |(maxB0 <= maxR1) & (maxG0 <= maxR1)| (todo en bytes)
-
-
-
-
-
-
-
- 					; xmm6 = | phiR_0 | phiG_0 | phiB_0 | caca |
- 					; xmm7 = | phiR_1 | phiG_1 | phiB_1 | caca |
-
-
-
-	; USAR BLEND
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- add rdi,8
- add rsi,8
- add r15, 1
-
- ; me fijo si estoy en el borde
- cmp r15,(ancho-2)/2  ; para ancho = 8, x = 3, para ancho = 12,
- jne continuar
- add rsi, 8 ; corrijo rsi
- add rdi,8
-
- continuar:
-
- loop
-
- ret
-
+;    xor r9, r9                      ; r9 = 0
+;    mov r9d, ecx                    ; r9 = alto
+;
+;    xor r10, r10   		            ; r10 =  0
+;    mov r10d, edx  		            ; r10 = ancho
+;    shl r10, 2     		            ; r10 = ancho*4
+;    
+;    xor r8, r8
+;    mov rax, r10					; rax = ancho*4
+;    mul 3							; rax = (ancho*4)*3
+;    mov r8, rax						; r8 = (ancho*4)*3
+;    
+;    xor r11, r11                 	; r10 =  0
+;    mov r11d, edx                	; r10 = ancho
+;    shr r11, 2						; r10 = ancho/4
+;    
+;
+;	; movemos rsi a la fila de arriba
+;	add rsi, r10
+;
+;
+;	; r9 va a ser un iterador = alto
+;    xor r9, r9                              ; r9 = 0
+;    mov r9d, ecx                            ; r9 = alto
+;
+;    ; seteo el contador
+;    mov rcx, r11                            ; rbx = ancho/4
+;    
+;    ; xmm0 =  |           |           |           |   alpha   |
+;    pshufd xmm0, 0000000000b
+;    ; xmm0 =  |   alpha   |   alpha   |   alpha   |   alpha   |
+;		
+;	movups xmm10, [uno]
+;	; xmm10 = |    1.0    |    1.0    |    1.0    |    1.0    |
+;	
+;	addps xmm10, xmm0	
+;	; xmm10 = | 1 + alpha | 1 + alpha | 1 + alpha | 1 + alpha |
+;	
+;	movups xmm11, [uno]
+;	; xmm11 = |    1.0    |    1.0    |    1.0    |    1.0    |
+;	
+;	subps xmm11, xmm0	
+;    ; xmm11 = | 1 - alpha | 1 - alpha | 1 - alpha | 1 - alpha |
+;	
+; .ciclo:
+;
+; 	movdqu xmm0, [rdi]
+; 	; xmm0 = | A[0][3] | R[0][3] | G[0][3] | B[0][3] | A[0][2] | R[0][2] | G[0][2] | B[0][2] | A[0][1] | R[0][1] | G[0][1] | B[0][1] | A[0][0] | R[0][0] | G[0][0] | B[0][0] |
+;    ;         127       119       111       103       95        87        79        71        63        55        47        39        31        23        15        7       0
+;	
+;	movdqu xmm1, [rdi + r10]
+;	; xmm1 = | A[1][3] | R[1][3] | G[1][3] | B[1][3] | A[1][2] | R[1][2] | G[1][2] | B[1][2] | A[1][1] | R[1][1] | G[1][1] | B[1][1] | A[1][0] | R[1][0] | G[1][0] | B[1][0] |
+;    ;         127       119       111       103       95        87        79        71        63        55        47        39        31        23        15        7       0
+;    
+;	movdqu xmm2, [rdi + 2*r10]
+;	; xmm2 = | A[2][3] | R[2][3] | G[2][3] | B[2][3] | A[2][2] | R[2][2] | G[2][2] | B[2][2] | A[2][1] | R[2][1] | G[2][1] | B[2][1] | A[2][0] | R[2][0] | G[2][0] | B[2][0] |
+;    ;         127       119       111       103       95        87        79        71        63        55        47        39        31        23        15        7       0
+;    
+;    pmaxub xmm1,xmm2
+;	; xmm1 = | max(pix[1][3], pix[2][3]) | max(pix[1][2], pix[2][2]) | max(pix[1][1], pix[2][1]) | max(pix[1][0], pix[2][0]) |
+;	
+;	
+;	; backupeo las 3 filas porque necesito los pixels del medio
+;	movdqu xmm3, xmm0
+;	; xmm3 = | A[0][3] | R[0][3] | G[0][3] | B[0][3] | A[0][2] | R[0][2] | G[0][2] | B[0][2] | A[0][1] | R[0][1] | G[0][1] | B[0][1] | A[0][0] | R[0][0] | G[0][0] | B[0][0] |
+;    ;         127       119       111       103       95        87        79        71        63        55        47        39        31        23        15        7       0
+;	
+;	movdqu xmm4, xmm1
+;	; xmm4 = | A[1][3] | R[1][3] | G[1][3] | B[1][3] | A[1][2] | R[1][2] | G[1][2] | B[1][2] | A[1][1] | R[1][1] | G[1][1] | B[1][1] | A[1][0] | R[1][0] | G[1][0] | B[1][0] |
+;    ;         127       119       111       103       95        87        79        71        63        55        47        39        31        23        15        7       0
+;    
+;	movdqu xmm5, xmm2
+;	; xmm5 = | A[2][3] | R[2][3] | G[2][3] | B[2][3] | A[2][2] | R[2][2] | G[2][2] | B[2][2] | A[2][1] | R[2][1] | G[2][1] | B[2][1] | A[2][0] | R[2][0] | G[2][0] | B[2][0] |
+;    ;         127       119       111       103       95        87        79        71        63        55        47        39        31        23        15        7       0
+;    
+;	
+;	; calculo maximos por columnas
+;	pmaxub xmm0,xmm1
+;	; xmm0 = | max(pix[0][3], pix[1][3], pix[2][3]) | max(pix[0][2], pix[1][2], pix[2][2]) | max(pix[0][1], pix[1][1], pix[2][1]) | max(pix[0][0], pix[1][0], pix[2][0]) |
+;	; xmm0 = | max(col3]) | max(col2) | max(col1) | max(col0) |
+; 	
+; 	movdqu xmm1, xmm0
+; 	; xmm1 = | max(col3) | max(col2) | max(col1) | max(col0) |
+; 	
+;	pshufb xmm0, [maxCol0y2_maxCol1y3]
+;	; xmm0 = |     0     |     0     | max(col3) | max(col2)  |
+;
+;	pmaxub xmm0, xmm1
+;	; xmm0 = |   fruta   |   fruta   | max(col1, col3) | max(col0, col2) |
+;	
+;	
+;	; ahora acomodo mis backups xmm3 y xmm5 para calcular los maximos con los pixeles del medio
+;	; quiero maximo entre: max(col0, col2), xmm3[1] y xmm5[1] (donde xmmn[k] es el k-ésimo pixel del registro xmmn)
+;	; y ademas quiero maximo entre: max(col1, col3), xmm3[2] y xmm5[2] (donde xmmn[k] es el k-ésimo pixel del registro xmmn)
+;	; entiéndase que anoto pixels, pero los máximos son realmente a nivel byte/componente
+;	
+;	; tengo:
+;	; xmm0 = |   fruta   |   fruta   | max(col1, col3) | max(col0, col2) |
+;	; xmm3 = |  xmm3[3]  |  xmm3[2]  |     xmm3[1]     |     xmm3[0]     |
+;	; xmm5 = |  xmm5[3]  |  xmm5[2]  |     xmm5[1]     |     xmm5[0]     |
+;	
+;	; entonces, acomodo mis backups para usar pmaxub nuevamente:
+;	
+;	movdqu xmm6, xmm3
+;	; xmm6 = |  xmm3[3]  |  xmm3[2]  |     xmm3[1]     |     xmm3[0]     |
+;	
+;	pshufb xmm6, [maxPixelsMedio]
+;	; xmm6 = |     0     |     0     |     xmm3[2]     |     xmm3[1]     |
+;	
+;	pmaxub xmm0, xmm6
+;	; xmm0 = |   fruta   |   fruta   | max(col1, col3, xmm3[2]) | max(col0, col2, xmm3[1]) |
+;	
+;	movdqu xmm6, xmm5
+;	; xmm6 = |  xmm5[3]  |  xmm5[2]  |     xmm5[1]     |     xmm5[0]     |
+;	
+;	pshufb xmm6, [maxPixelsMedio]
+;	; xmm6 = |     0     |     0     |     xmm5[2]     |     xmm5[1]     |
+;	
+;	pmaxub xmm0, xmm6
+;	; xmm0 = |   fruta   |   fruta   | max(col1, col3, xmm3[2], xmm5[2]) | max(col0, col2, xmm3[1], xmm5[1]) |
+;	
+;	; tengo:
+;	; xmm0 = |   fruta   |   fruta   | max*(p1) | max*(p0) |
+;	; xmm0 = |   fruta   |   fruta   | maxA1 | maxR1 | maxG1 | maxB1 | maxA0 | maxR0 | maxG0 | maxB0 |
+;	;                                 63      55      47      39      31      23      15      7     0
+;	
+;	; if (maxR >= maxG && maxR >= maxB){
+;	; 	res = 1 + alpha;
+;	; }
+;	; else
+;	; {
+;	; 		res = 1 - alpha;
+;	; }
+;	
+;	; if (maxR < maxG && maxG >= maxB){
+;	; 	res = 1 + alpha;
+;	; }
+;	; else
+;	; {
+;	; 		res = 1 - alpha;
+;	; }
+;	
+;	; if (maxR < maxB && maxG < maxB){
+;	; 	res = 1 + alpha;
+;	; }
+;	; else
+;	; {
+;	; 		res = 1 - alpha;
+;	; }
+;
+;	
+;	
+;	; calculemos phiR, phiG, phiB
+;	; extiendo xmm0 para poder comparar
+;	; xmm0 = |     0     |     0     | maxB1 | maxR1 | maxG1 | maxB1 | maxB0 | maxR0 | maxG0 | maxB0 |
+;	;                                 63      55      47      39      31      23      15      7     0
+;	
+;	
+;	
+;	; reacomodo xmm0 para aprovechar mas comparaciones
+;	
+;	pshufb xmm0, [mask1]
+;	; xmm0 = |     0     |     0     | maxB1 | maxR1 | maxG1 | maxB1 | maxB0 | maxR0 | maxG0 | maxB0 |
+;	;                                 63      55      47      39      31      23      15      7     0
+;	
+;	movdqu xmm1, xmm0
+;	; xmm1 = |     0     |     0     | maxA1 | maxR1 | maxG1 | maxB1 | maxA0 | maxR0 | maxG0 | maxB0 |
+;	;                                 63      55      47      39      31      23      15      7     0
+;	
+;	pshufb xmm1, [mask2]
+;	; xmm1 = |     0     |     0     | maxG1 | maxG1 | maxR1 | maxR1 | maxG0 | maxG0 | maxR0 | maxR0 |
+;	;                                 63      55      47      39      31      23      15      7     0
+;	
+;	pcmpgtb xmm0, xmm1	; xmm0 > xmm1
+;	
+;	; xmm0 = |         0         | maxB1 > maxG1 | maxR1 > maxG1 | maxG1 > maxR1 | maxB1 > maxR1 | maxB0 > maxG0 | maxR0 > maxG0 | maxG0 > maxR0 | maxB0 > maxR0 |
+;	;         127                 63              55              47              39              31              23              15             7              0
+;	
+;	; niego la mascara, porque así me sirve para phiR
+;	mov xmm2, xmm0
+;	xor xmm2, [maskUnos]
+;	
+;	; xmm4 = | maxG1 <= maxR1 | maxB1 <= maxR1 | maxB1 <= maxG1 | maxG1 <= maxB1| basurax4 | maxG0 <= maxR1| maxB0 <= maxR1| maxB0 <= maxG1 | maxG0 <= maxB1 | basurax4 |
+;	
+;	
+;	mov xmm6, xmm4				;xmm6 = xmm4
+;	mov xmm7,xmm4				; xmm7 = xmm4
+;	pshufb xmm6,[mascaraYaNoSeQueNombrePonerle1]
+;		pshufb xmm7,[mascaraYaNoSeQueNombrePonerle2]
+;	;xmm6 = | | | maxB1 <= maxR1 | maxB0 <= maxR1| (todo en bytes)
+;	;xmm7 = | | | maxG1 <= maxR1 | maxG0 <= maxR1| (todo en bytes)
+;	
+;	
+;	; xmm15 = | 1 + alpha | 1 - alpha | 1 + alpha | 1 - alpha |
+;	
+;	
+;	
+;	pand xmm6,xmm7
+;	;xmm6 = | | | (maxB1 <= maxR1) & (maxG1 <= maxR1) |(maxB0 <= maxR1) & (maxG0 <= maxR1)| (todo en bytes)
+;
+;
+;
+;
+;
+;
+;
+; 					; xmm6 = | phiR_0 | phiG_0 | phiB_0 | caca |
+; 					; xmm7 = | phiR_1 | phiG_1 | phiB_1 | caca |
+;
+;
+;
+;	; USAR BLEND
+;
+;
+;
+;
+;
+;
+;
+;
+;
+;
+;
+;
+;
+;
+; add rdi,8
+; add rsi,8
+; add r15, 1
+;
+; ; me fijo si estoy en el borde
+; cmp r15,(ancho-2)/2  ; para ancho = 8, x = 3, para ancho = 12,
+; jne continuar
+; add rsi, 8 ; corrijo rsi
+; add rdi,8
+;
+; continuar:
+;
+; loop
+;
+; ret
+;
 
--- tp2/codigo/filtros/combinar_asm.asm
+++ tp2/codigo/filtros/combinar_asm.asm
@@ -1,35 +1,218 @@
-; void combinar_asm (
-; 	unsigned char *src,
-; 	unsigned char *dst,
-; 	int cols,
-; 	int filas,
-; 	int src_row_size,
-; 	int dst_row_size,
-; 	float alpha
-; );
-
-; Parámetros:
-; 	rdi = src
-; 	rsi = dst
-; 	rdx = cols
-; 	rcx = filas
-; 	r8 = dst_row_size
-; 	r9 = dst_row_size
-; 	xmm0 = alpha
-
 global combinar_asm
-
-extern combinar_c
-
+ 
+section .rodata
+ 
+mascInversa:      db 0x0c,0x0d,0x0e,0x0f,0x08,0x09,0x0a,0x0b,0x04,0x05,0x06,0x07,0x00,0x01,0x02,0x03
+numero255: dd 255.0,255.0,255.0,255.0
+ 
+; void combinar_asm    (unsigned char *src, unsigned char *dst, int cols, int filas,
+;                     int src_row_size, int dst_row_size,float alpha);
+; Parámetros:
+;   rdi = src
+;   rsi = dst
+;   rdx = cols
+;   rcx = filas
+;   r8 = src_row_size
+;   r9 = dst_row_size
+;   xmm0 = alpha
+ 
 section .text
-
+ 
 combinar_asm:
-	;; TODO: Implementar
-
-	sub rsp, 8
-
-	call combinar_c
-
-	add rsp, 8
-
-	ret
+  push rbp ;A
+  mov rbp,rsp
+  push r10;D
+  push r11;A
+  push r12;D
+  push r13;A
+  push r15;D
+  sub rsp,8;A
+ 
+ 
+mov r10,rdi
+mov r11,rsi
+mov r13,rcx ; r13=filas
+xor r12,r12
+mov r12d,edx
+shl r12,2 ; r12=cols*4
+add r11,r12;
+ 
+xor rcx,rcx
+xor r12,r12
+mov r12d,edx
+shr r12,2 ; r12=cols/4
+mov ecx,r12d
+;     ------------
+;     ------------
+;     ------------
+;     ------------
+;  r10^       r11^
+ 
+ 
+  movdqu xmm5,[mascInversa]
+  mov r15,r13
+  .ciclo:
+    sub r11,16
+    movdqu xmm1,[r10]
+    pshufb xmm1,xmm5
+    movdqu [r11],xmm1
+    add r10,16
+    loop .ciclo
+mov r12d,edx
+shl r12,2 ; r12=cols*4
+add r11,r12;
+add r11,r12;
+ 
+xor rcx,rcx
+xor r12,r12
+mov r12d,edx
+shr r12,2 ; r12=cols/4
+mov ecx,r12d
+ 
+sub r15,1
+ 
+cmp r15,0
+jne .ciclo
+ 
+ 
+;TENGO EN DESTINO LA FOTO ESPEJADA
+;TENGO EN XMM0 ALPHA(FLOAT 4BYTES)
+movups xmm1,xmm0
+shufps xmm0,xmm1,0x00 ; xmm0=|alpha|alpha|alpha|alpha|
+movups xmm8,[numero255] ; xmm8=|255.0|255.0|255.0|255.0
+divps xmm0,xmm8         ;xmm0=|alpha/255.0|alpha/255.0|alpha/255.0|alpha/255.0|
+ 
+ 
+xor rcx,rcx
+mov r10d,edx
+shr r10d,2 ; r10=ancho/4
+mov r11d,r13d; r11=filas
+ 
+mov eax,r13d
+mul r10d
+mov ecx,eax
+;ecx=(ancho/4)*(filas)
+ pxor xmm12,xmm12
+ 
+.ciclo2:
+ 
+;AGARRO FOTO ORIGINAL
+  movdqu xmm1,[rdi] ;xmm1=|pixel3|pixel2|pixel1|pixel0
+ 
+movdqu xmm3,xmm1
+;CONVIERTO BYTES A WORDS FOTO ORIGINAL
+punpcklbw xmm1,xmm12;xmm1=| sa1  sg1 |  sr1  sb1| sa0  sg0 |  sr0  sb0| s=source
+punpckhbw xmm3,xmm12;xmm2=| sa3  sg3 |  sr3  sb3| sa2  sg2 |  sr2  sb2|
+ 
+;final = 76543210
+;xmm1=1-0
+;xmm3=3-2
+ 
+;CONVIERTO WORDS A 32 BITS INT
+movdqu xmm2,xmm1
+punpcklwd xmm1,xmm12
+punpckhwd xmm2,xmm12
+;xmm1=0
+;xmm2=1
+;xmm3=3-2
+ 
+movdqu xmm4,xmm3
+punpcklwd xmm3,xmm12
+punpckhwd xmm4,xmm12
+ 
+;xmm1=0
+;xmm2=1
+;xmm3=2
+;xmm4=3
+ 
+ 
+ 
+ 
+ ;AGARRO FOTO ESPEJADA
+  movdqu xmm5,[rsi]
+  movdqu xmm7,xmm5
+ ;CONVIERTO BYTES A WORDS FOTO ESPEJADA
+punpcklbw xmm5,xmm12
+punpckhbw xmm7,xmm12
+ 
+;xmm5=1-0
+;xmm7=3-2
+ 
+;CONVIERTO WORDS A 32 BITS INT
+movdqu xmm6,xmm5
+punpcklwd xmm5,xmm12
+punpckhwd xmm6,xmm12
+;xmm5=0
+;xmm6=1
+;xmm7=3-2
+ 
+movdqu xmm8,xmm7
+punpcklwd xmm7,xmm12
+punpckhwd xmm8,xmm12
+ 
+;xmm5=0
+;xmm6=1
+;xmm7=2
+;xmm8=3
+ 
+ 
+;Hago la resta de int 32 bits
+ 
+psubd xmm1,xmm5
+psubd xmm2,xmm6
+psubd xmm3,xmm7
+psubd xmm4,xmm8
+ 
+;CONVIERTO A FLOAT
+cvtdq2ps xmm1,xmm1
+cvtdq2ps xmm2,xmm2
+cvtdq2ps xmm3,xmm3
+cvtdq2ps xmm4,xmm4
+ 
+ 
+;MULTIPLICO POR ALPHA/255.0
+mulps xmm1,xmm0
+mulps xmm2,xmm0
+mulps xmm3,xmm0
+mulps xmm4,xmm0
+ 
+ 
+ 
+;CONVIERTO DE FLOAT A INT 32BITS
+CVTPS2DQ xmm1,xmm1
+CVTPS2DQ xmm2,xmm2
+CVTPS2DQ xmm3,xmm3
+CVTPS2DQ xmm3,xmm4
+ 
+ 
+;Le sumo la espejada
+ 
+paddd xmm1,xmm5
+paddd xmm2,xmm6
+paddd xmm3,xmm7
+paddd xmm4,xmm8
+ 
+;CONVIERTO DE INT 32 A SHORT
+ 
+packusdw xmm1,xmm2
+packusdw xmm3,xmm4
+ 
+;CONVIERTO A BYTE
+ 
+PACKUSWB xmm1,xmm3
+ 
+  movdqu [rsi],xmm1
+  add rdi,16
+  add rsi,16
+ 
+  DEC ECX
+  JNZ .ciclo2
+ 
+add rsp,8;D
+pop r15;A
+pop r13;D
+pop r12;A
+pop r11;D
+pop r10;A
+pop rbp;d
+ret;a
