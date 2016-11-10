5dfb57dbff6a44581107c0e772110fcf40e18837
Raposeiras Lucas Damian <lraposeiras@ws4.labo2>
2016-11-10 15:39:26 -0300

    Acomodado un poco el código en sched.c

--- tp3/entregable/tp3-bundle.v1/src/sched.c
+++ tp3/entregable/tp3-bundle.v1/src/sched.c
@@ -5,14 +5,10 @@
   definicion de funciones del scheduler
 */
 
-
-
-
-
-
 #include "sched.h"
-  #include "defines.h"
-  #include "i386.h"
+#include "defines.h"
+#include "i386.h"
+
 char corriendoBanderas;
 unsigned int tareaActual;
 unsigned int banderaActual;
@@ -20,194 +16,100 @@ char contadorTareas;
 char contadorBanderas;
 char cantTareasVivas;
 
-
-
-
 int indicesTareas[8];
 int indicesBanderas[8];
-
-
-
-
-
-
-
-
-
-
-
 
 void sched_inicializar() {
 
 	int i;
-  for(i=0;i<8;i++){
-    indicesTareas[i] = (GDT_IDX_T1_DESC+i)<<3;
-  }
-  for(i=0;i<8;i++){
-    indicesBanderas[i]= (GDT_IDX_T1_FLAG_DESC+i)<<3;
-  }
+	for(i=0;i<8;i++)
+	{
+		indicesTareas[i] = (GDT_IDX_T1_DESC+i)<<3;
+	}
+	for(i=0;i<8;i++)
+	{
+		indicesBanderas[i]= (GDT_IDX_T1_FLAG_DESC+i)<<3;
+	}
 
-
-
-
-tareaActual = -1;
-banderaActual = -1;
+	tareaActual = -1;
+	banderaActual = -1;
 	corriendoBanderas=0;
-contadorTareas=0;
+	contadorTareas=0;
 	contadorBanderas=0;
-cantTareasVivas=8;
-
-
-
-
+	cantTareasVivas=8;
 }
-
-
-
-
-
-
-
 
 unsigned short sched_proximo_indice() {
 
+	if (cantTareasVivas != 0)
+	{
 
 
+		if(!corriendoBanderas)
+		{
 
+			// Me fijo si ya ejecuté 3 tareas
+			if(++contadorTareas==4)
+			{
+				// Ya corrí 3 tareas
+				// Empiezo a correr banderas
+				corriendoBanderas=1;
+				contadorTareas = 0;
 
-if (cantTareasVivas != 0)
-{
+				return indicesBanderas[buscarIndiceSiguienteViva(1)]; // El 1 en indicesBanderas indica que busque en banderas
+			}
+			else
+			{
+				// Busco la siguiente tarea a ejecutar
+				return indicesTareas[buscarIndiceSiguienteViva(0)];
+			}
 
+		}
+		else
+		{
 
+			if(++contadorBanderas == cantTareasVivas)
+			{
+				//Termine de correr banderas
+				// Busco la siguiente tarea a ejecutar
+				corriendoBanderas = 0;
+				contadorBanderas=0;
 
+				return indicesTareas[buscarIndiceSiguienteViva(0)];
+			}
+			else
+			{
+				// Busco la siguiente bandera a ejecutar
+				return indicesBanderas[buscarIndiceSiguienteViva(1)]; // El 1 en indicesBanderas indica que busque en banderas
+			}
 
-if(!corriendoBanderas){
-
-
-
-
-	// Me fijo si ya ejecuté 3 tareas
-	if(++contadorTareas==4){
-	corriendoBanderas=1;
-	contadorTareas = 0;
-
-	return indicesBanderas[buscarIndiceSiguienteViva(1)] ;//El 1 en buscar indica que busque en banderas
-
-
-
-
-}else{
-	// Busco la siguiente tarea a ejecutar
-return indicesTareas[buscarIndiceSiguienteViva(0)];
+	}
+	else
+	{
+		return 0;
+	}
 }
-
-
-
-
-
-
-
-
-
-}else{
-
-
-
-
-
-
-
-
-	if(++contadorBanderas == cantTareasVivas){
-//Termine de correr banderas
-// Busco la siguiente tarea a ejecutar
-corriendoBanderas = 0;
-contadorBanderas=0;
-return indicesTareas[buscarIndiceSiguienteViva(0)];
-}
-	// Busco la siguiente bandera a ejecutar
-	return indicesBanderas[buscarIndiceSiguienteViva(1)] ;//El 1 en buscar indica que busque en banderas
-}
-
-}
-
-
-
-
-
-
-
-
-
-
-
-
-    return 0;
-}
-
-
-
-
-
-
 
 
 // Devuelve el índice en el arreglo de la siguiente tarea/bandera viva
 unsigned char buscarIndiceSiguienteViva(char esBandera)
 {
-
-
-
-
-
-
-
-
 	if (esBandera)
-{
-banderaActual = (banderaActual + 1)%8;
-while (indicesBanderas[banderaActual] == 0)
-{
-	banderaActual = (banderaActual  + 1)%8;
-
-
-
-
-}
-return banderaActual;
-
-
-
-
-
-
-
-
-}
-else
-{
-
-tareaActual = (tareaActual + 1)%8;
-while (indicesTareas[tareaActual] == 0)
-{
-	tareaActual = (tareaActual + 1)%8;
-
-
-
-
-}
-return tareaActual;
-
-
-
-
-
-
-
-
-}
-
-
-
-
+	{
+		banderaActual = (banderaActual + 1)%8;
+		while (indicesBanderas[banderaActual] == 0)
+		{
+			banderaActual = (banderaActual  + 1)%8;
+		}
+		return banderaActual;
+	}
+	else
+	{
+		tareaActual = (tareaActual + 1)%8;
+		while (indicesTareas[tareaActual] == 0)
+		{
+			tareaActual = (tareaActual + 1)%8;
+		}
+		return tareaActual;
+	}
 }
